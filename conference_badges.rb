f75fa9a921393f22d4563aae440aaf4a37332390
Jack Yeung <jyeung.se@gmail.com>
2018-06-18 19:03:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |name| 
+    puts "Hello, my name is #{name}."
+  end
+end
